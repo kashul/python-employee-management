528874c58aa7b6856f5f0b214598474ad2769a80
aba <you@example.com>
2020-02-17 01:34:30 +0530

    final commit

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+
+
 from . import views
 
 urlpatterns = [
     path('list', views.lists, name='list'),
     path('form/<int:id>', views.forms, name='update'),
     path('delete/<int:id>', views.delete, name='delete')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- manageEmployee/urls.py
+++ manageEmployee/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('log.urls'))
 ]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
